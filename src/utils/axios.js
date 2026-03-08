1e148d571187e5d463bc6fd36ce369566ec0da30
Nazreen Banu <nazreenb2512@gmail.com>
2026-03-08 16:03:13 +0530

    Updated backend URL for production and axios config

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3500/api",
+  baseURL: import.meta.env.VITE_BACKEND_URL + "/api",
 });
 
 // 🔐 Automatically attach token
